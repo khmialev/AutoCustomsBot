77478624f8d1f0b384b7e7c5c3e3f294a2c9cbc1
tema <akhmialev@gmail.com>
2025-03-24 14:41:25 +0300

    feat(usa_car): add new state

--- models/fsm_states.py
+++ models/fsm_states.py
@@ -3,16 +3,12 @@ from aiogram.fsm.state import StatesGroup, State
 
 class CopartCalcStates(StatesGroup):
     waiting_for_url = State()
-    waiting_for_brand = State()
-    waiting_for_engine = State()
-    waiting_for_year = State()
+    waiting_for_price_under_3_years = State()
 
 
 class IaaiCalcStates(StatesGroup):
     waiting_for_url = State()
-    waiting_for_brand = State()
-    waiting_for_engine = State()
-    waiting_for_year = State()
+    waiting_for_price_under_3_years = State()
 
 
 class ManualBasicCalcStates(StatesGroup):
