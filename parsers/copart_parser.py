f4175eb84d616db274823820680a1c14e672f320
tema <akhmialev@gmail.com>
2025-03-25 23:47:04 +0300

    feat(usa_car): add data for lot images

--- parsers/copart_parser.py
+++ parsers/copart_parser.py
@@ -15,6 +15,12 @@ class CopartParser(BasicParser):
             self.url = self.url.replace("ru/", "")
         car_code = self.url.split("/")[4]
         copart_url = f"{COPART_URL}{car_code}"
+        images_data = await self.get_copart_lot_images(
+            lot_id=car_code, referer=self.url, proxy=self.use_proxy
+        )
+        images = [
+            image["highResUrl"] for image in images_data["data"]["imagesList"]["IMAGE"]
+        ]
         car_data = await self.get_copart_json(
             copart_url=copart_url, referer=self.url, proxy=self.use_proxy
         )
@@ -23,10 +29,19 @@ class CopartParser(BasicParser):
         brand = car_data["data"]["lotDetails"]["mkn"]
         model = car_data["data"]["lotDetails"]["lmg"]
         year = car_data["data"]["lotDetails"]["lcy"]
-        image = car_data["data"]["lotDetails"]["tims"]
+        image = (
+            car_data["data"]["lotDetails"]["tims"]
+            .replace("lpp", "ids-c-prod-lpp")
+            .replace("_thb", "_hrs")
+        )
         engine = float(
             car_data["data"]["lotDetails"]["egn"].split(" ")[0].replace("L", "").strip()
         )
+
+        buy_now = car_data["data"]["lotDetails"]["dynamicLotDetails"]["buyTodayBid"]
+        current_bid = car_data["data"]["lotDetails"]["dynamicLotDetails"]["currentBid"]
+        sales_status = car_data["data"]["lotDetails"]["dynamicLotDetails"]["saleStatus"]
+
         await self.close_session()
 
         if "class" in model.lower():
@@ -49,4 +64,8 @@ class CopartParser(BasicParser):
             engine=engine,
             url=self.url,
             image=image,
+            buy_now=buy_now,
+            current_bid=current_bid,
+            sales_status=sales_status,
+            images=images,
         )
