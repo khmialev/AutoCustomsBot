4b1821d2e171570cf683e58264c50772f96a8884
tema <akhmialev@gmail.com>
2025-03-26 23:13:19 +0300

    feat(usa_car): add proxy

--- parsers/iaai_parser.py
+++ parsers/iaai_parser.py
@@ -7,12 +7,13 @@ class IaaiParser(BasicParser):
     def __init__(self, iaai_url: str):
         super().__init__()
         self.url = iaai_url
+        self.use_proxy: bool = False
 
     async def get_data(self):
         await self.get_session()
         car_code = self.url.split("/")[-1].split("~")[0]
         iaai_url = f"{IAAI_URL}{car_code}"
-        car_data = await self.get_iaai_json(iaai_url=iaai_url)
+        car_data = await self.get_iaai_json(iaai_url=iaai_url, proxy=self.use_proxy)
         if not car_data:
             return False
         brand = car_data["MakeName"]
@@ -20,14 +21,21 @@ class IaaiParser(BasicParser):
         year = car_data["ModelYear"]
         salvage_id = car_data["SalvageId"]
         branch_number = car_data["AdministrativeBranchNumber"]
-        image_url = f"https://vis.iaai.com/resizer?imageKeys={salvage_id}~SID~B{branch_number}~S0~I1~RW2576~H1932~TH0&width=845&height=633"
-        dirty_engine = await self.get_iaai_engine(iaai_url=self.url)
+        image_url = f"https://vis.iaai.com/resizer?imageKeys={salvage_id}~SID~B{branch_number}~S0~I1~RW2576~TH0&width=845&height=633"
+        images = [
+            f"https://vis.iaai.com/resizer?imageKeys={salvage_id}~SID~B{branch_number}~S0~I{i}~RW2576~TH0&width=845&height=633"
+            for i in range(1, 10)
+        ]
+
+        dirty_engine = await self.get_iaai_engine(
+            iaai_url=self.url, proxy=self.use_proxy
+        )
         if not dirty_engine:
             return False
         engine = float(dirty_engine.split(" ")[0].lower().replace("l", ""))
 
-        if brand == "BMW":
-            model: str = f"{model[0]}-seriya"
+        if "SERIES" in brand.lower():
+            model = model.lower().replace("series", "seriya").replace(" ", "-")
 
         await self.close_session()
         return AuctionCar(
@@ -37,4 +45,5 @@ class IaaiParser(BasicParser):
             engine=engine,
             url=self.url,
             image=image_url,
+            images=images,
         )
