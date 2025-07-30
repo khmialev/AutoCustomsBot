ac5023fa43d272c2185bcc6aae1f5597b670efe3
tema <akhmialev@gmail.com>
2025-07-30 17:20:44 +0300

    feat(car_bot): add states for set data for calculate car

---
+++ src/bot/states/calculation_customs_fee_states.py
@@ -0,0 +1,12 @@
+from aiogram.fsm.state import StatesGroup, State
+
+
+class CustomFeesStates(StatesGroup):
+    waiting_for_delivery_through_georgia = State()
+    waiting_for_delivery_through_lithuania = State()
+    waiting_for_auction_fee = State()
+    waiting_for_declarant_fee = State()
+    waiting_for_beneficiary_fee = State()
+    waiting_for_customs_duty = State()
+    waiting_for_recycling_fee = State()
+    waiting_for_auction_play_fee = State()
