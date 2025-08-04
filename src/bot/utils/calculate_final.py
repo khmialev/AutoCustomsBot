042751fb6ec3f5e54d226f02c17c4330d9b72b6d
tema <akhmialev@gmail.com>
2025-08-04 16:06:59 +0300

    feat(car_bot): add utils for calculate

--- src/bot/utils/calculate_final.py
+++ src/bot/utils/calculate_final.py
@@ -4,8 +4,10 @@ from aiogram.types import Message, InputMediaPhoto
 
 from calculation.calculator import CalculateLogic
 from models.CalculatorCarModel import CalculateCar
-from parsers.AvParser import av_statistic
-from parsers.models.CarModel import AuctionCar, AvStatisticCar
+
+from parsers.AvAnalyticsParser import av_analytics
+from parsers.AvStatisticParser import av_statistic, AvStatistic
+from parsers.models.CarModel import AuctionCar, AvAnalyticsCar
 from src.bot.utils.TextGenerator import text_generator
 
 
@@ -18,10 +20,18 @@ async def process_final_car_data(
 
     car_calculate: CalculateCar = await CalculateLogic().calculate(
         car_manufacture_year=auction_car.year,
-        engine_volume=auction_car.engine,
+        engine_volume=auction_car.engine_capacity,
         car_price=estimated_price,
     )
-    av_statistic_car: AvStatisticCar = await av_statistic.run(
+    av_analytics_car: AvAnalyticsCar = await av_analytics.run(
+        brand=auction_car.brand,
+        model=auction_car.model,
+        year=auction_car.year,
+        engine_type=auction_car.engine_type,
+        engine_capacity=auction_car.engine_capacity,
+    )
+
+    av_statistic_car: AvStatistic = await av_statistic.run_parser(
         brand=auction_car.brand,
         model=auction_car.model,
         year=auction_car.year,
@@ -31,6 +41,7 @@ async def process_final_car_data(
         web_car=auction_car,
         car_calculate=car_calculate,
         estimated_price=estimated_price,
+        av_analytics_car=av_analytics_car,
         av_statistic_car=av_statistic_car,
     )
 
@@ -48,7 +59,7 @@ async def process_final_car_data(
                 car_calculate=car_calculate,
                 estimated_price=estimated_price,
                 msg_to_long=True,
-                av_statistic_car=av_statistic_car,
+                av_analytics_car=av_analytics_car,
             )
             await message.answer_photo(
                 photo=auction_car.image,
