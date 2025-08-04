06985e6d79eed1b8bba379157f0443271887e953
tema <akhmialev@gmail.com>
2025-08-04 15:48:26 +0300

    feat(car_bot): add headres for av

---
+++ parsers/heders/av_headers.py
@@ -0,0 +1,10 @@
+AV_HEADERS = {
+    "accept": "*/*",
+    "accept-language": "ru-RU,ru;q=0.9,en-US;q=0.8,en;q=0.7",
+    "content-type": "application/json",
+    "origin": "https://cars.av.by",
+    "priority": "u=1, i",
+    "referer": "https://cars.av.by/",
+    "sec-ch-ua-mobile": "?0",
+    "sec-fetch-site": "same-site",
+}
