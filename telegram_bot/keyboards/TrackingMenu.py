3a1fffb9bcf4156f069b9155fc152f6f6b9b1aba
tema <akhmialev@gmail.com>
2025-10-24 12:31:46 +0300

    feat(car_bot): add new keyboards

--- telegram_bot/keyboards/TrackingMenu.py
+++ telegram_bot/keyboards/TrackingMenu.py
@@ -1,9 +1,20 @@
-from aiogram.types import ReplyKeyboardMarkup, KeyboardButton
+import json
+from datetime import datetime
 
+from aiogram.types import ReplyKeyboardMarkup, KeyboardButton, InlineKeyboardButton, InlineKeyboardMarkup
+
+def load_cars():
+    with open("./cars_structured.json", "r", encoding="utf-8") as file:
+        cars = json.load(file)
+        return cars
+
+cars = load_cars()
 
 async def tracking_menu_keyboard():
     return ReplyKeyboardMarkup(
         keyboard=[
+            [KeyboardButton(text="🚗 Запустить")],
+            [KeyboardButton(text="💥 Остановить.")],
             [KeyboardButton(text="📋 Список отслеживания")],
             [KeyboardButton(text="➕ Добавить авто")],
             [KeyboardButton(text="❌ Удалить авто")],
@@ -12,3 +23,77 @@ async def tracking_menu_keyboard():
         ],
         resize_keyboard=True,
     )
+
+
+
+
+async def create_brands_keyboard_bidcars():
+    buttons = []
+    brands = set()
+    for car in cars:
+        make = car.get("make", "").strip()
+        if make and make not in brands:
+            brands.add(make)
+            buttons.append(
+                [InlineKeyboardButton(text=make, callback_data=f"brand:{make}")]
+            )
+
+    return InlineKeyboardMarkup(inline_keyboard=buttons)
+
+async def create_models_keyboards_bidcars(brand: str):
+    buttons = []
+
+    for car in cars:
+        if car.get("make").lower() == brand.lower():
+            buttons.append(
+                [
+                    InlineKeyboardButton(
+                        text=car.get("model").upper(),
+                        callback_data=f"model:{car.get("model")}",
+                    )
+                ]
+            )
+    return InlineKeyboardMarkup(inline_keyboard=buttons)
+
+
+async def create_years_keyboard_bidcars(model: str):
+    # todo какой пзц подумаю как улучшить
+    buttons = []
+    for car in cars:
+        if car.get("model", "").lower() == model.lower():
+            generations = car.get("generations")
+
+            if generations and len(generations) == 1 and generations[0] is None:
+                generations = None
+
+            if generations:
+                for gen in generations:
+                    if not gen:
+                        continue
+                    text = f"{gen.get('name', 'Неизвестно')} ({gen.get('min_year', '?')}-{gen.get('max_year', '?')})"
+                    callback = (
+                        f"{gen.get('min_year', '0')}:{gen.get('max_year', '0')}"
+                    )
+                    buttons.append(
+                        [InlineKeyboardButton(text=text, callback_data=callback)]
+                    )
+            else:
+                year_count = car.get("year_count", [])
+                if year_count:
+                    buttons.append(
+                        [
+                            InlineKeyboardButton(
+                                text="All generation",
+                                callback_data=f"all:1900:{datetime.now().year}",
+                            )
+                        ]
+                    )
+                else:
+                    buttons.append(
+                        [
+                            InlineKeyboardButton(
+                                text=f"{model.upper()} (нет данных)",
+                                callback_data="unknown_year",
+                            )
+                        ]
+                    )
