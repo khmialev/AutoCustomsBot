f16c74bf87cba19f1c20978c3feb4d661a1c9f46
tema <akhmialev@gmail.com>
2025-10-24 14:00:30 +0300

    feat(car_bot): add menu

--- telegram_bot/keyboards/TrackingMenu.py
+++ telegram_bot/keyboards/TrackingMenu.py
@@ -1,10 +1,13 @@
 import json
+import os
 from datetime import datetime
 
 from aiogram.types import ReplyKeyboardMarkup, KeyboardButton, InlineKeyboardButton, InlineKeyboardMarkup
 
 def load_cars():
-    with open("./cars_structured.json", "r", encoding="utf-8") as file:
+    project_root = os.path.dirname(os.path.dirname(os.path.dirname(__file__)))
+    file_path = os.path.join(project_root, "cars_structured.json")
+    with open(file_path, "r", encoding="utf-8") as file:
         cars = json.load(file)
         return cars
 
@@ -13,17 +16,23 @@ cars = load_cars()
 async def tracking_menu_keyboard():
     return ReplyKeyboardMarkup(
         keyboard=[
-            [KeyboardButton(text="🚗 Запустить")],
-            [KeyboardButton(text="💥 Остановить.")],
-            [KeyboardButton(text="📋 Список отслеживания")],
-            [KeyboardButton(text="➕ Добавить авто")],
-            [KeyboardButton(text="❌ Удалить авто")],
-            [KeyboardButton(text="💳 Изменить тариф (Отслеживание)")],
-            [KeyboardButton(text="⬅️ Назад в меню")],
+            [
+                KeyboardButton(text="🚗 Запустить"),
+                KeyboardButton(text="🛑 Остановить отслеживание"),
+                KeyboardButton(text="📋 Список отслеживания"),
+            ],
+            [
+                KeyboardButton(text="➕ Добавить авто"),
+                KeyboardButton(text="❌ Удалить авто"),
+            ],
+            [
+                KeyboardButton(text="💳 Изменить тариф (Отслеживание)"),
+                KeyboardButton(text="⬅️ Назад в меню"),
+            ],
         ],
         resize_keyboard=True,
+        input_field_placeholder="Выберите действие 🚗",
     )
-
 
 
 
@@ -31,11 +40,10 @@ async def create_brands_keyboard_bidcars():
     buttons = []
     brands = set()
     for car in cars:
-        make = car.get("make", "").strip()
-        if make and make not in brands:
-            brands.add(make)
+        if car and car not in brands:
+            brands.add(car)
             buttons.append(
-                [InlineKeyboardButton(text=make, callback_data=f"brand:{make}")]
+                [InlineKeyboardButton(text=car, callback_data=f"brand:{car}")]
             )
 
     return InlineKeyboardMarkup(inline_keyboard=buttons)
@@ -43,57 +51,60 @@ async def create_brands_keyboard_bidcars():
 async def create_models_keyboards_bidcars(brand: str):
     buttons = []
 
-    for car in cars:
-        if car.get("make").lower() == brand.lower():
-            buttons.append(
-                [
-                    InlineKeyboardButton(
-                        text=car.get("model").upper(),
-                        callback_data=f"model:{car.get("model")}",
-                    )
-                ]
-            )
+    models = cars.get(brand)
+    for model in models:
+        buttons.append(
+            [
+                InlineKeyboardButton(
+                    text=model,
+                    callback_data=f"model:{model}",
+                )
+            ]
+        )
     return InlineKeyboardMarkup(inline_keyboard=buttons)
 
 
-async def create_years_keyboard_bidcars(model: str):
+async def create_years_keyboard_bidcars(data:dict):
     # todo какой пзц подумаю как улучшить
     buttons = []
-    for car in cars:
-        if car.get("model", "").lower() == model.lower():
-            generations = car.get("generations")
+    models = cars.get(data['brand'])
+    generations = models.get(data['model'], {}).get("generations", [])
 
-            if generations and len(generations) == 1 and generations[0] is None:
-                generations = None
 
-            if generations:
-                for gen in generations:
-                    if not gen:
-                        continue
-                    text = f"{gen.get('name', 'Неизвестно')} ({gen.get('min_year', '?')}-{gen.get('max_year', '?')})"
-                    callback = (
-                        f"{gen.get('min_year', '0')}:{gen.get('max_year', '0')}"
+    # if generations and len(generations) == 1 and generations[0] is None:
+    #     generations = None
+
+    if generations:
+        for gen in generations:
+            if not gen:
+                continue
+            text = f"{gen.get('name', 'Неизвестно')} ({gen.get('min_year', '?')}-{gen.get('max_year', '?')})"
+            callback = (
+                f"{gen.get('min_year', '0')}:{gen.get('max_year', '0')}"
+            )
+            buttons.append(
+                [InlineKeyboardButton(text=text, callback_data=callback)]
+            )
+    else:
+        year_count = models.get("year_count", [])
+        if year_count:
+            buttons.append(
+                [
+                    InlineKeyboardButton(
+                        text="All generation",
+                        callback_data=f"all:1900:{datetime.now().year}",
                     )
-                    buttons.append(
-                        [InlineKeyboardButton(text=text, callback_data=callback)]
+                ]
+            )
+        else:
+            buttons.append(
+                [
+                    InlineKeyboardButton(
+                        text=f"{data['model']} (нет данных)",
+                        callback_data="unknown_year",
                     )
-            else:
-                year_count = car.get("year_count", [])
-                if year_count:
-                    buttons.append(
-                        [
-                            InlineKeyboardButton(
-                                text="All generation",
-                                callback_data=f"all:1900:{datetime.now().year}",
-                            )
-                        ]
-                    )
-                else:
-                    buttons.append(
-                        [
-                            InlineKeyboardButton(
-                                text=f"{model.upper()} (нет данных)",
-                                callback_data="unknown_year",
-                            )
-                        ]
-                    )
+                ]
+            )
+
+    return InlineKeyboardMarkup(inline_keyboard=buttons)
+
