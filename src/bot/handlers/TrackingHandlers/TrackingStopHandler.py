7f3f9db6e25bc10adbf592e0d9019a801300bddd
tema <akhmialev@gmail.com>
2025-10-24 14:00:40 +0300

    feat(car_bot): add stop logic

--- src/bot/handlers/TrackingHandlers/TrackingStopHandler.py
+++ src/bot/handlers/TrackingHandlers/TrackingStopHandler.py
@@ -1,12 +1,24 @@
 from aiogram import Router, F
 from aiogram.types import Message
 
+from src.bot.services.TrackingManager import tracking_manager
+from telegram_bot.keyboards.MainMenu import main_menu_keyboard
 
 router = Router(name="tracking-stop-router")
 
 
-@router.message(F.text == "💥 Остановить.")
-async def add_car(message: Message):
-    await message.answer(
-        "Трекинг остановлен.",
-    )
+@router.message(F.text == "🛑 Остановить отслеживание")
+async def stop_tracking_command(message: Message):
+    user_id = message.from_user.id
+
+    if tracking_manager._is_tracking(user_id):
+        await tracking_manager.stop_tracking(user_id)
+        await message.answer(
+            "❌ Отслеживание остановлено.",
+            reply_markup=await main_menu_keyboard()
+        )
+    else:
+        await message.answer(
+            "ℹ️ У вас нет активного отслеживания.",
+            reply_markup=await main_menu_keyboard(),
+        )
