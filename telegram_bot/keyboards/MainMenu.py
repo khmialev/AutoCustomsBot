1ecac739bd804adacdb5df1bd7f9d1ccdc2a211f
tema <akhmialev@gmail.com>
2025-10-24 13:59:49 +0300

    feat(car_bot): fix size

--- telegram_bot/keyboards/MainMenu.py
+++ telegram_bot/keyboards/MainMenu.py
@@ -1,13 +1,19 @@
 from aiogram.types import ReplyKeyboardMarkup, KeyboardButton
 
 
+
 async def main_menu_keyboard():
     return ReplyKeyboardMarkup(
         keyboard=[
-            [KeyboardButton(text="🚗 Отслеживание авто")],
-            [KeyboardButton(text="💰 Расчет авто")],
-            [KeyboardButton(text="📦 Мои подписки")],
-            [KeyboardButton(text="ℹ️ Помощь")],
+            [
+                KeyboardButton(text="🚗 Отслеживание авто"),
+                KeyboardButton(text="💰 Расчет авто"),
+            ],
+            [
+                KeyboardButton(text="📦 Мои подписки"),
+                KeyboardButton(text="ℹ️ Помощь"),
+            ],
         ],
         resize_keyboard=True,
+        input_field_placeholder="Выберите раздел ⚙️",
     )
