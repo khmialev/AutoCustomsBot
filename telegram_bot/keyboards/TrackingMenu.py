7f09903eb9a5d1e0aa14818b690b4ba5769b4d70
tema <akhmialev@gmail.com>
2025-11-02 22:56:24 +0300

    feat(car_bot): add new keyboards

--- telegram_bot/keyboards/TrackingMenu.py
+++ telegram_bot/keyboards/TrackingMenu.py
@@ -41,14 +41,11 @@ async def tracking_menu_keyboard():
 
 async def create_brands_keyboard_bidcars():
     buttons = []
-    brands = set()
+
     for car in cars:
-        make = car.get("make", "").strip()
-        if make and make not in brands:
-            brands.add(make)
-            buttons.append(
-                [InlineKeyboardButton(text=make, callback_data=f"brand:{make}")]
-            )
+        buttons.append(
+            [InlineKeyboardButton(text=car, callback_data=f"brand:{car}")]
+        )
 
     return InlineKeyboardMarkup(inline_keyboard=buttons)
 
@@ -56,61 +53,58 @@ async def create_brands_keyboard_bidcars():
 async def create_models_keyboards_bidcars(brand: str):
     buttons = []
 
-    for car in cars:
-        if car.get("make").lower() == brand.lower():
+    brands = cars.get(brand, [])
+    for model in brands.keys():
+        buttons.append(
+            [
+                InlineKeyboardButton(
+                    text=model,
+                    callback_data=f"model:{model}",
+                )
+            ]
+        )
+    return InlineKeyboardMarkup(inline_keyboard=buttons)
+
+
+async def create_years_keyboard_bidcars(brand: str, model: str):
+    # todo какой пзц подумаю как улучшить
+    buttons = []
+    generations = cars.get(brand, []).get(model, [])
+    if generations and len(generations) == 1 and generations[0] is None:
+        generations = None
+    if generations:
+        for gen in generations:
+            name = gen.get("name", "Неизвестно")
+            years = gen.get("years", "Неизвестно")
+            if years != "Неизвестно":
+                year_from, year_to = years.split(
+                    "–"
+                )  # тут не "-", а длинное тире
+            else:
+                year_from, year_to = "?", "?"
+            text = f"{name} ({year_from} - {year_to})"
+            callback = f"{year_from} - {year_to}"
+            buttons.append(
+                [InlineKeyboardButton(text=text, callback_data=callback)]
+            )
+    else:
+        year_count = generations.get("year_count", [])
+        if year_count:
             buttons.append(
                 [
                     InlineKeyboardButton(
-                        text=car.get("model").upper(),
-                        callback_data=f"model:{car.get('model')}",
+                        text="All generation",
+                        callback_data=f"all:1900:{datetime.now().year}",
+                    )
+                ]
+            )
+        else:
+            buttons.append(
+                [
+                    InlineKeyboardButton(
+                        text=f"{model.upper()} (нет данных)",
+                        callback_data="unknown_year",
                     )
                 ]
             )
     return InlineKeyboardMarkup(inline_keyboard=buttons)
-
-
-async def create_years_keyboard_bidcars(model: str):
-    # todo какой пзц подумаю как улучшить
-    buttons = []
-    for car in cars:
-        if car.get("model", "").lower() == model.lower():
-            generations = car.get("generations")
-
-            if generations and len(generations) == 1 and generations[0] is None:
-                generations = None
-
-            if generations:
-                for gen in generations:
-                    if not gen:
-                        continue
-                    text = f"{gen.get('name', 'Неизвестно')} ({gen.get('min_year', '?')}-{gen.get('max_year', '?')})"
-                    callback = (
-                        f"{gen.get('min_year', '0')}:{gen.get('max_year', '0')}"
-                    )
-                    buttons.append(
-                        [
-                            InlineKeyboardButton(
-                                text=text, callback_data=callback
-                            )
-                        ]
-                    )
-            else:
-                year_count = car.get("year_count", [])
-                if year_count:
-                    buttons.append(
-                        [
-                            InlineKeyboardButton(
-                                text="All generation",
-                                callback_data=f"all:1900:{datetime.now().year}",
-                            )
-                        ]
-                    )
-                else:
-                    buttons.append(
-                        [
-                            InlineKeyboardButton(
-                                text=f"{model.upper()} (нет данных)",
-                                callback_data="unknown_year",
-                            )
-                        ]
-                    )
