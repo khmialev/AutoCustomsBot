66c47c1cf7cd7972055aa445abb8c68ae1f0c9c9
tema <akhmialev@gmail.com>
2025-11-03 14:03:07 +0300

    feat(car_bot): add tracking repo

--- src/database/repositories/repository_manager.py
+++ src/database/repositories/repository_manager.py
@@ -1,4 +1,5 @@
 from src.database.repositories.manager import OrmRepositoryManager
+from src.database.repositories.tracking_repository import TrackingRepository
 from src.database.repositories.user_repository import UserRepository
 
 
@@ -6,4 +7,5 @@ class RepositoryManager(OrmRepositoryManager):
     async def __aenter__(self):
         # создаём конкретные репозитории
         self.users = UserRepository(self._session)
+        self.tracking = TrackingRepository(self._session)
         return self
